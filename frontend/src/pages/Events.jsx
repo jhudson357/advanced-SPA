1849721de6a7faa746ef4bf831e16df83c42cd34
James Hudson <jmh357@comcast.net>
2023-03-14 14:13:32 -0500

    had error handling to events page

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -3,8 +3,13 @@ import { useLoaderData } from 'react-router-dom'
 import EventsList from '../components/EventsList'
 
 function EventsPage() {
-  const events = useLoaderData()
-
+  const data = useLoaderData()
+  
+  // if (data.isError) {
+  //   return <p>{data.message}</p>
+  // }
+  const events = data.events
+  
   return (
     <>
       <EventsList events={events} />
@@ -14,13 +19,15 @@ function EventsPage() {
 
 export default EventsPage
 
+// code in loader executes in the browser, not in backend server --> so you can use any browser APIs in the loader fxn (localStorage, cookies, etc.
+// can't use React hooks -> bc it's not in the react component)
 export async function loader() {
   const response = await fetch('http://localhost:8080/events')
 
   if (!response.ok) {
-    // ...
+    // return {isError: true, message: 'Count not fetch events.'}
+    throw { message: 'Could not fetch events.' }
   } else {
-    const resData = await response.json()
-    return resData.events
+    return response
   }
 }
