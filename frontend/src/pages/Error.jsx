02e4056249f95679cd2b71cfde4c3a2efeaf5323
James Hudson <jmh357@comcast.net>
2023-03-14 14:28:56 -0500

    add additional error handling

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,13 +1,29 @@
+import { useRouteError } from "react-router-dom"
+
 import MainNavigation from "../components/MainNavigation"
+import PageContent from "../components/PageContent"
 
 const ErrorPage = () => {
+  const error = useRouteError()
+
+  let title = 'An error occured!'
+  let message = 'Something went wrong.'
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!'
+    message = 'Could not find resource or page.'
+  }
+
   return (
     <>
       <MainNavigation />
-      <div>
-        <h1>An error occured!</h1>
-        <p>Could not find this page.</p>
-      </div>
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   )
 }
