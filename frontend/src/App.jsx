6f1a95c60786895fdf92aa1352c3b97948302e4a
James Hudson <jmh357@comcast.net>
2023-03-14 15:42:27 -0500

    implement create newEvent functionality using action function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import RootLayout from './pages/Root'
 import HomePage from './pages/Home'
 import EventsPage, {loader as eventsLoader} from './pages/Events'
 import EventDetailPage, {loader as eventDetailLoader} from './pages/EventDetail'
-import NewEventPage from './pages/NewEvent'
+import NewEventPage, {action as newEventAction} from './pages/NewEvent'
 import EditEventPage from './pages/EditEvent'
 import ErrorPage from './pages/Error'
 import EventsRootLayout from './pages/EventsRoot'
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
               {path: 'edit', element: <EditEventPage />},
             ]
           },
-          {path: 'new', element: <NewEventPage />},
+          {path: 'new', element: <NewEventPage />, action: newEventAction},
         ]
       }
     ]
