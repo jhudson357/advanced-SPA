247869a1add06f8d8da4f80d0fbfc8b9f86ee094
James Hudson <jmh357@comcast.net>
2023-03-14 17:02:56 -0500

    defer data fetching with defer

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,29 +1,24 @@
-import { useLoaderData, json } from 'react-router-dom'
+import { Suspense } from 'react'
+import { useLoaderData, json, defer, Await } from 'react-router-dom'
 
 import EventsList from '../components/EventsList'
 
 function EventsPage() {
-  const data = useLoaderData()
-  
-  // if (data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events
+  const {events} = useLoaderData()
   
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   )
 }
 
 export default EventsPage
 
 
-
-// code in loader executes in the browser, not in backend server --> so you can use any browser APIs in the loader fxn (localStorage, cookies, etc.
-// can't use React hooks -> bc it's not in the react component)
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events')
 
   if (!response.ok) {
@@ -34,6 +29,15 @@ export async function loader() {
       { status: 500 },
     )
   } else {
-    return response
+    const responseData = await response.json()
+    return responseData.events
   }
+}
+
+// code in loader executes in the browser, not in backend server --> so you can use any browser APIs in the loader fxn (localStorage, cookies, etc.
+// can't use React hooks -> bc it's not in the react component)
+export function loader() {
+  return defer({
+    events: loadEvents()
+  })
 }
